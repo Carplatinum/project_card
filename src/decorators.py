5d7965f1e7ad5e81909f5c0f0fe07fa861826d63
ANTAQ <mymillions@ya.ru>
2025-03-11 18:50:20 +0300

    Создание модуля decorator.py

---
+++ src/decorators.py
@@ -0,0 +1,31 @@
+import logging
+import functools
+import sys
+from datetime import datetime
+
+def log(filename=None):
+    """
+    Декоратор для логирования функций.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+            except Exception as e:
+                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                result = None
+
+            if filename:
+                with open(filename, 'a') as file:
+                    file.write(log_message + "\n")
+            else:
+                print(log_message)
+
+            return result
+
+        return wrapper
+
+    return decorator
