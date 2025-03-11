07e08da124dc84f6732a6e29b94d551acd32a280
ANTAQ <mymillions@ya.ru>
2025-03-11 19:34:17 +0300

    Создание модуля test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,20 +1,20 @@
 import pytest
 from decorators import log
-from io import StringIO
-import sys
+from typing import Any
+from pathlib import Path
 
 
 # Фикстура для теста вывода в консоль
 @pytest.fixture
-def capsys(capsys):
+def capsys(capsys: Any) -> Any:
     return capsys
 
 
-def test_log_console(capsys):
+def test_log_console(capsys: Any) -> None:
     """Тест логирования в консоль."""
 
     @log()
-    def test_function(x, y):
+    def test_function(x: int, y: int) -> int:
         return x + y
 
     test_function(1, 2)
@@ -22,12 +22,12 @@ def test_log_console(capsys):
     assert "test_function ok" in captured.out
 
 
-def test_log_file(tmp_path):
+def test_log_file(tmp_path: Path) -> None:
     """Тест логирования в файл."""
     filename = tmp_path / "test_log.txt"
 
     @log(filename=str(filename))
-    def test_function(x, y):
+    def test_function(x: int, y: int) -> int:
         return x + y
 
     test_function(1, 2)
@@ -36,11 +36,11 @@ def test_log_file(tmp_path):
     assert "test_function ok" in log_content
 
 
-def test_log_error_console(capsys):
+def test_log_error_console(capsys: Any) -> None:
     """Тест логирования ошибки в консоль."""
 
     @log()
-    def test_error_function(x, y):
+    def test_error_function(x: int, y: int) -> None:
         raise ValueError("Ошибка")
 
     try:
@@ -51,12 +51,12 @@ def test_log_error_console(capsys):
     assert "test_error_function error: ValueError" in captured.out
 
 
-def test_log_error_file(tmp_path):
+def test_log_error_file(tmp_path: Path) -> None:
     """Тест логирования ошибки в файл."""
     filename = tmp_path / "test_error_log.txt"
 
     @log(filename=str(filename))
-    def test_error_function(x, y):
+    def test_error_function(x: int, y: int) -> None:
         raise ValueError("Ошибка")
 
     try:
