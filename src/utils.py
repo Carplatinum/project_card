bf25c4b9ad94ea010f65d79ddb514e1e3541b451
ANTAQ <mymillions@ya.ru>
2025-03-19 06:30:54 +0300

    Правка модуля utils.py

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+from typing import List, Dict
+
+
+def read_json_file(file_path: str) -> List[Dict]:
+    """
+    Читает JSON-файл и возвращает список словарей с данными о финансовых транзакциях.
+
+    Если файл пустой, содержит не список или не найден, возвращается пустой список.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
